b04fb6f39bcc7711495681d1c95fb02aefa94aa6
Yashwanth1906 <yashwanthsakthivel01@gmail.com>
2024-12-16 22:40:17 +0530

    authentication-half-completed

---
+++ backend/src/auth/passport.ts
@@ -0,0 +1,84 @@
+import passport from "passport";
+//@ts-ignore
+import {Strategy as GoogleStartegy} from "passport-google-oauth20";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcryptjs"
+import { PrismaClient } from "@prisma/client";
+// import { prisma } from "./db";
+const prisma = new PrismaClient();
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID? process.env.GOOGLE_CLIENT_ID : "";
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET? process.env.GOOGLE_CLIENT_SECRET : "";
+
+console.log(GOOGLE_CLIENT_ID)
+
+export const initPassport =()=>{
+    passport.use(
+        new GoogleStartegy(
+            {
+                clientID : GOOGLE_CLIENT_ID || "87001147804-h6dkm6rev6b5pb53p2obtu4l2l1n2ihq.apps.googleusercontent.com",
+                clientSecret: GOOGLE_CLIENT_SECRET || "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",
+                callbackURL:"/api/v1/users/google/callback"
+            },async(accessToken :string,refreshToken : string,profile : any,done : any)=>{
+                console.log("Storing in db")
+                console.log(profile.emails[0].value);
+                console.log(profile.displayName)
+                const user = await prisma.user.upsert({
+                    //@ts-ignore
+                    create:{
+                        email : profile.emails[0].value,
+                        
+                        name : profile.displayName,
+                    },update:{
+                        name : profile.displayName
+                    }
+                    //@ts-ignore
+                    ,where:{
+                        email : profile.emails[0].value
+                    }
+                })
+                done(null,user);
+            }
+    )
+    )
+    passport.use('phone-login',             // this thing is used to login using phone number and maintain session for it . 
+        new LocalStrategy({                 // for that i used pasport-local in that we need to pass two fields ,
+            //@ts-ignore                    // which is usernameField and we need to map it with a unique field and passwordField.
+            usernameField : "phoneno",
+            passwordField : "password"
+        },async (phoneno,password,done) =>{
+            try{
+                const user = await prisma.user.findUnique({where:{phoneNo : phoneno}})
+                if(!user){
+                    return done(null,false,{message:"Invalid Phone Number"})
+                }
+                //@ts-ignore
+                const match = await bcrypt.compare(password,user.password);
+                if(!match){
+                    return done(null,false,{message:"Wrong credentials"})
+                }
+                return done(null,user);
+            } catch(e){
+                console.log(e)
+                return done(e);
+            }
+        }
+    )
+    )
+
+    passport.serializeUser((user : any,callback)=>{
+        process.nextTick(()=>{
+            return callback(null,{
+                id : user.id,
+                username : user.username,
+                // picture : user.picture
+            })
+        })
+    })
+
+    passport.deserializeUser((user : any,callback) =>{
+        process.nextTick(()=>{
+            return callback(null,user);
+        })
+    })
+}
