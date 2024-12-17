4838c4951de8438c8b492ce91b874c6f553a1b6f
MukeshG7172 <mukeshrjpm1@gmail.com>
2024-12-17 15:04:00 +0530

    Navbar Updated

---
+++ client/src/app/metadata.js
@@ -0,0 +1,11 @@
+import { Metadata } from 'next'
+
+export const baseMetadata = {
+  title: 'Your Site Title',
+  description: 'Your site description',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    // other OpenGraph properties
+  }
+}
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -51,21 +51,42 @@ const Navbar = () => {
       <div className="flex space-x-6">
         <button 
           onClick={() => scrollToSection('home')}
-          className="flex items-center space-x-2 text-[#1E90FF] hover:underline transition"
+          className={`
+            flex 
+            items-center 
+            space-x-2 
+            hover:underline 
+            transition
+            ${isScrolled ? 'text-[#1E90FF]' : 'text-white'}
+          `}
         >
           <Home size={20} />
           <span>Home</span>
         </button>
         <button 
           onClick={() => scrollToSection('about')}
-          className="flex items-center space-x-2 text-[#1E90FF] hover:underline transition"
+          className={`
+            flex 
+            items-center 
+            space-x-2 
+            hover:underline 
+            transition
+            ${isScrolled ? 'text-[#1E90FF]' : 'text-white'}
+          `}
         >
           <Info size={20} />
           <span>About</span>
         </button>
         <button 
           onClick={() => scrollToSection('contact')}
-          className="flex items-center space-x-2 text-[#318beb] hover:underline transition"
+          className={`
+            flex 
+            items-center 
+            space-x-2 
+            hover:underline 
+            transition
+            ${isScrolled ? 'text-[#318beb]' : 'text-white'}
+          `}
         >
           <Contact size={20} />
           <span>Contact</span>
