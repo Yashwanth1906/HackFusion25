05012882cbd80cacb3353273e84dbf8c7c77d4c0
Priya Dharsini P <dharsini15306@gmail.com>
2024-12-17 17:58:11 +0530

    image-bug fix

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -160,42 +160,43 @@ export default function About() {
 
               {/* Right Column - Carousel */}
               <motion.div 
-                initial={{ opacity: 0, scale: 0.9 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.5 }}
-                className="hidden md:block relative w-full h-[500px] overflow-visible rounded-xl"
-              >
-                <AnimatePresence mode="wait">
-                  <motion.img
-                    key={currentImageIndex}
-                    src={images[currentImageIndex]}
-                    alt={`Hackathon image ${currentImageIndex + 1}`}
-                    initial={{ opacity: 0, x: 100 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    exit={{ opacity: 0, x: -100 }}
-                    transition={{ 
-                      duration: 0.5,
-                      ease: "easeInOut"
-                    }}
-                    className="absolute inset-0 w-full h-full object-cover"
-                  />
-                </AnimatePresence>
+  initial={{ opacity: 0, scale: 0.9 }}
+  animate={{ opacity: 1, scale: 1 }}
+  transition={{ duration: 0.5 }}
+  className="relative w-full h-[500px] overflow-visible rounded-xl"
+>
+  <AnimatePresence mode="wait">
+    <motion.img
+      key={currentImageIndex}
+      src={images[currentImageIndex]}
+      alt={`Hackathon image ${currentImageIndex + 1}`}
+      initial={{ opacity: 0, x: 100 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: -100 }}
+      transition={{ 
+        duration: 0.5,
+        ease: "easeInOut"
+      }}
+      className="absolute inset-0 w-full h-full object-cover"
+    />
+  </AnimatePresence>
 
-                {/* Navigation dots */}
-                <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
-                  {images.map((_, index) => (
-                    <button
-                      key={index}
-                      onClick={() => setCurrentImageIndex(index)}
-                      className={`w-3 h-3 rounded-full transition-all duration-300 ${
-                        currentImageIndex === index 
-                          ? 'bg-blue-600 scale-125' 
-                          : 'bg-gray-300 hover:bg-blue-300'
-                      }`}
-                    />
-                  ))}
-                </div>
-              </motion.div>
+  {/* Navigation dots */}
+  <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+    {images.map((_, index) => (
+      <button
+        key={index}
+        onClick={() => setCurrentImageIndex(index)}
+        className={`w-3 h-3 rounded-full transition-all duration-300 ${
+          currentImageIndex === index 
+            ? 'bg-blue-600 scale-125' 
+            : 'bg-gray-300 hover:bg-blue-300'
+        }`}
+      />
+    ))}
+  </div>
+</motion.div>
+
             </div>
           </div>
 
